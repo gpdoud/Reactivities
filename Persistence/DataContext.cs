458cb5857133fa2b0e234ef99b1944ad2e7c4c64
Greg Doud <gpdoud@gmail.com>
2023-12-13 12:38:53 -0500

    End of section 2

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+    public DbSet<Domain.Activity> Activities { get; set; }
+}
