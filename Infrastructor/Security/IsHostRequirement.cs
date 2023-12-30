873d1ae6566440c1148aa0540673318834c8e2af
Greg Doud <gpdoud@gmail.com>
2023-12-30 14:45:17 -0500

    Readded .AsNoTracking()

--- Infrastructor/Security/IsHostRequirement.cs
+++ Infrastructor/Security/IsHostRequirement.cs
@@ -1,31 +1,29 @@
 ﻿using System.Security.Claims;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+
 using Persistence;
 
 namespace Infrastructor;
 
 
-public class IsHostRequirement : IAuthorizationRequirement
-{
+public class IsHostRequirement : IAuthorizationRequirement {
 
 }
 
-public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
-{
+public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement> {
     private readonly DataContext _dbContext;
     private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public IsHostRequirementHandler(DataContext dbContext, 
-        IHttpContextAccessor httpContextAccessor) 
-    {
+    public IsHostRequirementHandler(DataContext dbContext,
+        IHttpContextAccessor httpContextAccessor) {
         _dbContext = dbContext;
         _httpContextAccessor = httpContextAccessor;
     }
 
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
-    {
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement) {
         var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
 
         if(userId == null) return Task.CompletedTask;
@@ -34,13 +32,13 @@ public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
             .SingleOrDefault(x => x.Key == "id").Value?.ToString());
 
         var attendee = _dbContext.ActiviteyAttendees
-            //.AsNoTracking() // not needed with current softeare
+            .AsNoTracking()
             .SingleOrDefaultAsync(x => x.AppUserId == userId && x.ActivityId == activiteyId).Result;
 
         if(attendee == null) return Task.CompletedTask;
 
         if(attendee.IsHost) context.Succeed(requirement);
 
-        return Task.CompletedTask; 
+        return Task.CompletedTask;
     }
 }
